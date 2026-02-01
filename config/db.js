ef71c8ba60d4a782974aa1401f15886e1bc19d8e
Bahaa-Fadel1 <bahaazaki652@gmail.com>
2026-02-01 18:43:52 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
-const mysql = require('mysql2');
+const { Pool } = require("pg");
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'newsDB'
+const pool = new Pool({
+  host: "xyzcompany.supabase.co",  // انسخ host من Supabase
+  user: "postgres",
+  password: "2312617@BBBBB@",     // كلمة السر اللي أنشأتها
+  database: "postgres",
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database Connected');
+pool.connect(err => {
+  if (err) console.error("❌ Database Connection Error:", err);
+  else console.log("✅ Supabase Connected");
 });
 
-module.exports = db;
+module.exports = pool;
